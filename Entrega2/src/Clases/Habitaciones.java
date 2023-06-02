4b5e6bbbc65d9396277e0de46a10f0c6da3cb02d
Nicole <nv.martinezv1@uniandes.edu.co>
2023-06-02 12:20:44 -0500

    Cambio Habitación

--- Entrega2/src/Clases/Habitaciones.java
+++ Entrega2/src/Clases/Habitaciones.java
@@ -13,9 +13,25 @@ public class Habitaciones {
 	private String tipo;
 	private boolean vista;
 	private boolean cocina;
+	private int tamaño;
+	private boolean aireAcondicionado;
+	private boolean calefaccion;
+	private String tamañoCama;
+	private boolean TV;
+	private boolean cafetera;
+	private boolean ropaCamayTapetes;
+	private boolean plancha;
+	private boolean secadorPelo;
+	private int voltajeAC;
+	private boolean tomasUSB-A;
+	private boolean tomasUSB-C;
+	private boolean desayuno;
 
 	public Habitaciones(int numero, int tarifa, String ubicacion, 
-			boolean balcon, String tipo, boolean vista, boolean cocina) {
+			boolean balcon, String tipo, boolean vista, boolean cocina, int tamaño, boolean aireAcondicionado,
+			boolean calefaccion, String tamañoCama, boolean TV, boolean cafetera, boolean ropaCamayTapetes,
+			boolean plancha, boolean secadorPelo, int voltajeAC, boolean tomasUSB-A, boolean tomasUSB-C,
+			boolean desayuno) {
 		this.numero = numero;
 		this.tarifa = tarifa;
 		this.ubicacion = ubicacion;
@@ -23,59 +39,30 @@ public class Habitaciones {
 		this.tipo = tipo;
 		this.vista = vista;
 		this.cocina = cocina;
-	}
-	
-	public int getNumero() {
-		return numero;
-	}
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-	public int getTarifa() {
-		return tarifa;
-	}
-	public void setTarifa(int tarifa) {
-		this.tarifa = tarifa;
-	}
-	public String getUbicacion() {
-		return ubicacion;
-	}
-	public void setUbicacion(String ubicacion) {
-		this.ubicacion = ubicacion;
-	}
-	public boolean isBalcon() {
-		return balcon;
-	}
-	public void setBalcon(boolean balcon) {
-		this.balcon = balcon;
-	}
-	public String getTipo() {
-		return tipo;
-	}
-	public void setTipo(String tipo) {
-		this.tipo = tipo;
-	}
-	public boolean isVista() {
-		return vista;
-	}
-	public void setVista(boolean vista) {
-		this.vista = vista;
-	}
-	public boolean isCocina() {
-		return cocina;
-	}
-	public void setCocina(boolean cocina) {
-		this.cocina = cocina;
-	}
-	
-	public boolean existeHabitacion(int numero) {
-		return this.habitaciones.containsKey(numero);
+		this.tamaño = tamaño;
+		this.aireAcondicionado = aireAcondicionado;
+		this.calefaccion = calefaccion;
+		this.tamañoCama = tamañoCama;
+		this.TV = TV;
+		this.cafetera = cafetera;
+		this.ropaCamayTapetes = ropaCamayTapetes;
+		this.plancha = plancha;
+		this.secadorPelo = secadorPelo;
+		this.voltajeAC = voltajeAC;
+		this.tomasUSB-A = tomasUSB-A;
+		this.tomasUSB-C = tomasUSB-C;
+		this.desayuno = desayuno;
 	}
 	
 	public boolean crearHabitacion(int numero, int tarifa, String ubicacion, 
-			boolean balcon, String tipo, boolean vista, boolean cocina) {
+			boolean balcon, String tipo, boolean vista, boolean cocina, int tamaño, boolean aireAcondicionado,
+			boolean calefaccion, String tamañoCama, boolean TV, boolean cafetera, boolean ropaCamayTapetes,
+			boolean plancha, boolean secadorPelo, int voltajeAC, boolean tomasUSB-A, boolean tomasUSB-C,
+			boolean desayuno) {
 		if (!existeHabitacion(numero)) {
-			Habitaciones habitacion = new Habitaciones(numero, tarifa, ubicacion, balcon, tipo, vista, cocina);
+			Habitaciones habitacion = new Habitaciones(numero, tarifa, ubicacion, balcon, tipo, vista, cocina, tamaño, 
+					aireAcondicionado,calefaccion, tamañoCama, TV, cafetera, ropaCamayTapetes, plancha, secadorPelo, 
+					voltajeAC, tomasUSB-A, tomasUSB-C, desayuno);
 			this.habitaciones.put(numero, habitacion);
 			return true;
 		}else {
@@ -94,9 +81,14 @@ public class Habitaciones {
 	}
 	
 	public void editarHabitacion(int numero, int tarifa, String ubicacion, 
-			boolean balcon, String tipo, boolean vista, boolean cocina) {
+			boolean balcon, String tipo, boolean vista, boolean cocina, int tamaño, boolean aireAcondicionado,
+			boolean calefaccion, String tamañoCama, boolean TV, boolean cafetera, boolean ropaCamayTapetes,
+			boolean plancha, boolean secadorPelo, int voltajeAC, boolean tomasUSB-A, boolean tomasUSB-C,
+			boolean desayuno) {
 		this.habitaciones.remove(numero);
-		crearHabitacion(numero, tarifa, ubicacion, balcon, tipo, vista, cocina);
+		crearHabitacion(numero, tarifa, ubicacion, balcon, tipo, vista, cocina, tamaño, 
+				aireAcondicionado,calefaccion, tamañoCama, TV, cafetera, ropaCamayTapetes, plancha, secadorPelo, 
+				voltajeAC, tomasUSB-A, tomasUSB-C, desayuno);
 	}
 	
 	public Habitaciones consultarTarifa(int numero) {
