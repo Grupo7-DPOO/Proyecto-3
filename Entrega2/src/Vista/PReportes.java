36d0465b0633ed27d7c4f3365133c22b4ceeff32
Juan Sebastián Ortega Romero <jasurome@gmail.com>
2023-06-02 23:16:43 -0500

    FINAL

--- Entrega2/src/Vista/PReportes.java
+++ Entrega2/src/Vista/PReportes.java
@@ -1,4 +1,4 @@
-package Interfaz_para_clientes;
+package Vista;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
--- Entrega2/src/Vista/PTarjeta.java
+++ Entrega2/src/Vista/PTarjeta.java
@@ -1,9 +1,14 @@
+package Vista;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,16 +16,17 @@ import javax.swing.JTextField;
 
 public class PTarjeta extends JDialog {
 
-	private static final long serialVersionUID = 1L;
-	private FPrincipal fPrincipal;
+    private static final long serialVersionUID = 1L;
+    private FPrincipal fPrincipal;
     private JTextField tValor;
-	private JTextField tComercio;
-	private JTextField tNFactura;
-	private JTextField tFecha;
-	private JTextField tNTarjeta;
-	private JTextField tCaducidad;
-	private JTextField tCodigoSeguridad;
-    private JButton bPagar;
+    private JTextField tComercio;
+    private JTextField tNFactura;
+    private JTextField tFecha;
+    private JTextField tNTarjeta;
+    private JTextField tCaducidad;
+    private JTextField tCodigoSeguridad;
+    private JComboBox<String> comboBox;
+    private JButton bEnviar;
 
     public PTarjeta(FPrincipal fPrincipal) {
         this.setfPrincipal(fPrincipal);
@@ -31,7 +37,7 @@ public class PTarjeta extends JDialog {
         this.setLayout(new BorderLayout());
 
         JPanel pCampos = new JPanel();
-        pCampos.setLayout(new GridLayout(4, 4, 10, 10));
+        pCampos.setLayout(new GridLayout(5, 2, 10, 10));
         pCampos.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         JLabel lValor = new JLabel("Valor:");
@@ -65,13 +71,78 @@ public class PTarjeta extends JDialog {
         pCampos.add(lCodigoSeguridad);
         pCampos.add(tCodigoSeguridad);
 
+        JPanel pOpciones = new JPanel();
+        pOpciones.setLayout(new FlowLayout(FlowLayout.LEFT));
+        pOpciones.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JLabel lOpciones = new JLabel("Enviar a:");
+        String[] opciones = { "uniandes.dpoo.PayPal", "uniandes.dpoo.PayU", "uniandes.dpoo.Sire" };
+        comboBox = new JComboBox<>(opciones);
+
+        pOpciones.add(lOpciones);
+        pOpciones.add(comboBox);
+
         JPanel pBotones = new JPanel();
         pBotones.setLayout(new FlowLayout(FlowLayout.RIGHT));
         pBotones.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        bPagar = new JButton("Pagar");
-        pBotones.add(bPagar);
+        bEnviar = new JButton("Enviar");
+        bEnviar.addActionListener(e -> enviarInformacion());
+
+        pBotones.add(bEnviar);
 
         this.add(pCampos, BorderLayout.CENTER);
+        this.add(pOpciones, BorderLayout.NORTH);
         this.add(pBotones, BorderLayout.SOUTH);
     }
+
+    private void enviarInformacion() {
+        String opcionSeleccionada = comboBox.getSelectedItem().toString();
+        String valor = tValor.getText();
+        String comercio = tComercio.getText();
+        String nFactura = tNFactura.getText();
+        String fecha = tFecha.getText();
+        String nTarjeta = tNTarjeta.getText();
+        String caducidad = tCaducidad.getText();
+        String codigoSeguridad = tCodigoSeguridad.getText();
+
+        StringBuilder contenido = new StringBuilder();
+        contenido.append("Valor: ").append(valor).append("\n");
+        contenido.append("Comercio: ").append(comercio).append("\n");
+        contenido.append("Numero Factura: ").append(nFactura).append("\n");
+        contenido.append("Fecha: ").append(fecha).append("\n");
+        contenido.append("Numero Tarjeta: ").append(nTarjeta).append("\n");
+        contenido.append("Fecha Caducidad: ").append(caducidad).append("\n");
+        contenido.append("Codigo de Seguridad: ").append(codigoSeguridad).append("\n");
+
+        String nombreArchivo = "";
+        switch (opcionSeleccionada) {
+            case "uniandes.dpoo.PayPal":
+                nombreArchivo = "PayPal.txt";
+                break;
+            case "uniandes.dpoo.PayU":
+                nombreArchivo = "PayU.txt";
+                break;
+            case "uniandes.dpoo.Sire":
+                nombreArchivo = "Sire.txt";
+                break;
+            default:
+                System.out.println("Opción no válida.");
+                return;
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nombreArchivo))) {
+            writer.write(contenido.toString());
+            System.out.println("Archivo de registro creado exitosamente.");
+        } catch (IOException e) {
+            System.out.println("Error al crear el archivo de registro: " + e.getMessage());
+        }
+    }
+    public FPrincipal getfPrincipal() {
+        return fPrincipal;
+    }
+
+    public void setfPrincipal(FPrincipal fPrincipal) {
+        this.fPrincipal = fPrincipal;
+    }
+}
